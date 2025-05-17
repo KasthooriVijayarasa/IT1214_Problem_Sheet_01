3cb03233ab12370df370783aa2acaee20a707cfd
KasthooriVijayarasa <selvivijay0904@gmail.com>
2025-05-17 10:18:32 +0000

    done

---
+++ Student.java
@@ -0,0 +1,14 @@
+class Student{
+    private int studentid;
+    private String name;
+    private int daysAttended;
+
+    Student(int id , String na , int days){
+        studentid=id;
+        name=na;
+        daysAttended=days;
+    }
+
+    
+
+}
